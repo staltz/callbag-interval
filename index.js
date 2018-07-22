b9ea7967373617872a6c60fa690d319d6cc54492
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-07-22 14:20:28 +0200

    Use callbag-timer

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-const interval = period => (start, sink) => {
-  if (start !== 0) return;
-  let i = 0;
-  const id = setInterval(() => {
-    sink(1, i++);
-  }, period);
-  sink(0, t => {
-    if (t === 2) clearInterval(id);
-  });
-};
+const timer = require('callbag-timer');
+
+const interval = period => timer(period, period);
 
 module.exports = interval;
